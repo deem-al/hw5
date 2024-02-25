d56c82e84621e5290f761e56aeed6b1920bffc4c
deem-al <156614723+deem-al@users.noreply.github.com>
2024-02-25 06:38:54 -0600

    Update pa5.py

--- pa5.py
+++ pa5.py
@@ -10,3 +10,23 @@ def bisection_root(function, LB, UB):
         return LB
     if abs(function(UB)) < 1e-6:
         return UB
+
+#Problem 2: Dictionary Filter
+
+def dict_filter(f, d):
+    '''produces a new dictionary where a given key and value remain associated with each other 
+    in the new dictionary, if and only if the function returns True when called with the key and the value'''
+    
+    filtered_d = {}
+    
+    for key, value in d.items():
+    
+        if f(key,value):
+            filtered_d[key] = value
+        return filtered_d
+
+def checker(name, abbrev):
+    return abbrev[0] == "I" and name[1] == "l"
+example = {"Illinois": "IL", "Pennsylvania": "PA", "Indiana": "IN"}
+
+dict_filter(checker, example)
