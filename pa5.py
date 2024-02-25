1a2f3018ccc9d56ae784064327eca5c5addc8f50
deem-al <156614723+deem-al@users.noreply.github.com>
2024-02-25 06:20:51 -0600

    Add files via upload

---
+++ pa5.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#Problem 1: Bisection Method
+
+def bisection_root(function, LB, UB):
+    '''Uses bisection root method to find a root of a function'''
+    
+    if function(LB) * function(UB) > 0:
+        raise ValueError ("we cannot expect to find a root between two points on the same side of the xaxis")
+        
+    if abs(function(LB)) < 1e-6:
+        return LB
+    if abs(function(UB)) < 1e-6:
+        return UB
+
